b768c2ea16c9288daec9100967339fb2fa1797aa
Shrirang Saraogi <saraogishrirang@gmail.com>
2026-09-10 20:38:58 +0530

    added the add url button

--- public/script.js
+++ public/script.js
@@ -19,15 +19,18 @@ document.getElementById('formtoaddsong').addEventListener('submit', event => {
     event.preventDefault();
 
     const titleInput = document.getElementById('songtitle');
+    const linkInput = document.getElementById('urlofsong');
     const title = titleInput.value;
+    const link = linkInput.value;
     fetch('/queue', {
         method: 'POST',
         headers: {'Content-Type' : 'application/json'},
-        body: JSON.stringify({title: title})
+        body: JSON.stringify({title: title, url: link})
     })
     .then(res => res.json())
     .then(songs => {
         showqueue(songs);
     });
     titleInput.value = '';
+    linkInput.value = '';
 });
