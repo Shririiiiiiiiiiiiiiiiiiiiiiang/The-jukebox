cd4762c478db23f29a3b2fc3666474b4df3e341a
Shrirang Saraogi <saraogishrirang@gmail.com>
2026-09-10 20:08:52 +0530

    added the add song to queue button for now it is updated in the console only after update site it gets added here

--- public/script.js
+++ public/script.js
@@ -8,3 +8,20 @@ fetch('/queue')
         list.appendChild(item);
     });
 });
+
+document.getElementById('formtoaddsong').addEventListener('submit', event => {
+    event.preventDefault();
+
+    const titleInput = document.getElementById('songtitle');
+    const title = titleInput.value;
+    fetch('/queue', {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify({title: title})
+    })
+    .then(res => res.json())
+    .then(songs => {
+        console.log(songs);
+    });
+    titleInput.value = '';
+});
