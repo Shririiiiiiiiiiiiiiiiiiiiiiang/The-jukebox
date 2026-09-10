f9f7797d31af93185cb3c3e48312d94b6c8aa5f6
Shrirang Saraogi <saraogishrirang@gmail.com>
2026-09-10 18:13:01 +0530

    added the ul list for to keep ongs in html also now it can show songs on the webpage using <li>

--- public/script.js
+++ public/script.js
@@ -1,5 +1,10 @@
 fetch('/queue')
 .then(res => res.json())
 .then(songs => {
-    console.log(songs);
+    const list = document.getElementById('songlist');
+    songs.forEach(song => {
+        const item = document.createElement('li');
+        item.textContent = song.title;
+        list.appendChild(item);
+    });
 });
