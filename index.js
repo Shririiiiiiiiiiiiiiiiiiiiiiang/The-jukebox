43e7af3ebd083f192e35981e74122c77b3bd7281
Shrirang Saraogi <saraogishrirang@gmail.com>
2026-09-17 13:07:22 +0530

    trying to debug the issue

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const app = express();
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const path = require('path');
 
 app.use(express.json());
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 const PORT = process.env.PORT || 3000;
 
 
