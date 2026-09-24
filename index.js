67b47bfa23d840d6d6060e355cf47a8d165766eb
Shrirang Saraogi <saraogishrirang@gmail.com>
2026-09-24 10:59:43 +0530

    making a system to add files too

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { createClient } = require('@supabase/supabase-js');
 const app = express();
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 const path = require('path');
+const multer = require('multer');
+const upload = multer({storage: multer.memoryStorage()});
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -21,8 +23,28 @@ app.get('/queue', async (req, res) => {
     res.json(data);
 });
 
-app.post('/queue',async (req, res) => {
-const {title, url}  = req.body;
+
+
+
+
+app.post('/queue',upload.single('songfile'), async (req, res) => {
+const {title }  = req.body;
+let url= req.body.url;
+if(!req.file && !url) return res.status(400).json({error: 'enter a url or a file'})
+
+if(req.file) {
+    const filename = Date.now() + '-' + req.file.originalname.replace(/[^a-zA-Z0-9.]/g, '_'); //added the datenow just to put a costom timestam for to if two people upload same thing so it should not overwritee
+    const {error: uploadError} = await supabase.storage
+    .from('songs')
+    .upload(filename, req.file.buffer, {contentType: req.file.mimetype});
+    if(uploadError) 
+        return res.status(500).json({error: uploadError.message});
+    const {data: publicurldata} = supabase.storage.from('songs').getPublicUrl(filename);
+    url = publicurldata.publicUrl;
+}
+
+
+
 const { data, error } = await supabase.from('songs').insert([{title, url }]).select();
 if(error) 
     return res.status(500).json({error: error.message});
@@ -30,6 +52,12 @@ const{data: allSongs} = await supabase.from('songs').select('*').order('id');
     
     res.json(allSongs);
 })
+
+
+
+
+
+
 
 app.delete('/queue', async(req, res) => {
     const {error} = await supabase.from('songs').delete().neq('id', 0);
